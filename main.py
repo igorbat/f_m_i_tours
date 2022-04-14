0aaf6dab78407901270c457a24d563f584825202
igorbat <igorbat99@gmail.com>
2022-04-14 22:25:57 +0100

    14.04 tumen

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import discord
 from secret import TOKEN
 from discord.ext import commands
 from abaka.data.tasks_5_strong import *
-from abaka.data.tasks_5_weak import *
-from abaka.data.tasks_8_strong import *
-from abaka.data.tasks_8_weak import *
 
 from abaka.abaka_cls import *
 
@@ -14,37 +11,37 @@ bot = commands.Bot(command_prefix='!')
 state_machine = StateMachine()
 
 
-game_test = \
-    GameAbaka("test",
-              "https://docs.google.com/document/d/1VlLj1B6JeLmsBeVtijxZNSb5KWbF4nHI-uD7-zvZnwQ/edit?usp=sharing",
-              TASKS_TEST)
-state_machine.add_tour("test", game_test)
+# game_test = \
+#     GameAbaka("test",
+#               "https://docs.google.com/document/d/1VlLj1B6JeLmsBeVtijxZNSb5KWbF4nHI-uD7-zvZnwQ/edit?usp=sharing",
+#               TASKS_TEST)
+# state_machine.add_tour("test", game_test)
 
 game_strong_5 = \
     GameAbaka("pro_5",
-              "https://drive.google.com/file/d/1Aw00bK3RMkRLVxQPVVUnTMzpNe3wihFV/view?usp=sharing",
+              "https://drive.google.com/file/d/1iUoQsds4sFFd8eKD3LuC6AoOyvR5VHHK/view?usp=sharing",
               TASKS_5_STRONG)
 state_machine.add_tour("pro_5", game_strong_5)
 
-game_weak_5 = \
-    GameAbaka("novice_5",
-              "https://drive.google.com/file/d/1eXZjFVYTt9Iu9EQbZVulwQ4mYaHzUgeR/view?usp=sharing",
-              TASKS_5_WEAK)
-state_machine.add_tour("novice_5", game_weak_5)
+# game_weak_5 = \
+#     GameAbaka("novice_5",
+#               "https://drive.google.com/file/d/1eXZjFVYTt9Iu9EQbZVulwQ4mYaHzUgeR/view?usp=sharing",
+#               TASKS_5_WEAK)
+# state_machine.add_tour("novice_5", game_weak_5)
 
 
-game_strong_8 = \
-    GameAbaka("pro_8",
-              "https://drive.google.com/file/d/1Mjlc2-P1wdZw6WLuH6KPEML1CIz_Mrfb/view?usp=sharing",
-              TASKS_8_STRONG)
-state_machine.add_tour("pro_8", game_strong_8)
+# game_strong_8 = \
+#     GameAbaka("pro_8",
+#               "https://drive.google.com/file/d/1Mjlc2-P1wdZw6WLuH6KPEML1CIz_Mrfb/view?usp=sharing",
+#               TASKS_8_STRONG)
+# state_machine.add_tour("pro_8", game_strong_8)
 
 
-game_weak_8 = \
-    GameAbaka("novice_8",
-              "https://drive.google.com/file/d/1a9L0kT0yJs9VTFjIgxTaZO7ELQdOYcin/view?usp=sharing",
-              TASKS_8_WEAK)
-state_machine.add_tour("novice_8", game_weak_8)
+# game_weak_8 = \
+#     GameAbaka("novice_8",
+#               "https://drive.google.com/file/d/1a9L0kT0yJs9VTFjIgxTaZO7ELQdOYcin/view?usp=sharing",
+#               TASKS_8_WEAK)
+# state_machine.add_tour("novice_8", game_weak_8)
 
 
 @bot.command(name='register', help='зарегистрировать команду: школа, класс или целиком название')
